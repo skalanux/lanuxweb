a1c32e56dc273831cd74d09ada7a28b0bfd33f74
Juan Manuel Schillaci <ska@lanux.org.ar>
2017-06-17 18:07:38 -0300

    Updated link page for lists

--- lanuxsite/settings.py
+++ lanuxsite/settings.py
@@ -39,7 +39,8 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(INSTALL_DIR, "/site_media/")
+#MEDIA_ROOT = os.path.join(INSTALL_DIR, "/site_media/")
+MEDIA_ROOT = '/srv/virtualenvs/lanuxsite/code/lanuxsite'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
